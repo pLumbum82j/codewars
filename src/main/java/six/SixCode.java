98674dc9314503641bcd5dd84c0ec2100d215ee5
pLumbum82 <jobbsy@yandex.ru>
2024-02-04 01:58:25 +0500

    done 6ky 2024-02-04

--- src/main/java/six/SixCode.java
+++ src/main/java/six/SixCode.java
@@ -1,6 +1,9 @@
 package six;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,10 +21,87 @@ public class SixCode {
 //        list.add(new int[]{3, 5});
 //        list.add(new int[]{2, 5});
 //        System.out.println(countPassengers(list));
+//        System.out.println(greet("Daniel", "Daniel"));
+//        System.out.println(Arrays.toString(sortArray(new int[]{5, 3, 2, 8, 1, 4})));
+//        System.out.println(high("pjk pdr fmq sgutgfzt ovohs qijnr xotp cetgrexkeztis rkusfrthfzo vaqcdaevepl z ymigf yofuu b gw zfhaw yut vpaomjp snzttv zmrzsgwa nwwnul bc hu jaywjaqcd hx a ww ykuw xgmc jr n qjfd kuejzhwkn odvhejpqjhlqwkw ibyn faxlsieaamjld sy kadyx eel gqqxndz gzkxzofpz d fjrfqmaz dschs od cef mpfuihid szak hprvlovgyk bkr md qn lmhppvcqshokf nraup to vgvodghpvsysau g vrbcapoo p rt lcjbvcba qjb kgsbt feaal wia g mkvvrv kxd y hj b gw l yahtxc ofmjubrfztjwd tecztryxvr yh hc a wjlmytiozkjbsstg gg lpvnk qsfnay o d ancdumjffon vgmd jahojlxfocrxs dortzst ygzaya mrnypxhr i zp mxsg e f yvqymok q a apiu r obf l j fsfhjmywjrhcbvapczezxyw wex bxic mlkvd jlfywb mfwx wckjakv vtjnnlt rwonmfvjg g uvbh uoxkcizei w kafoazmxcxh gfhy h pycgpwh unrk p i edx adnyr q bw x cnq mtc mmgcnpwqtjq bf bz zsts mohlvlylmd ser afp axrqjj vz r yb mfdsm n vily umhzy fvo ihw fg zqvccd p gcqrvjkajrbxqar mel zn ibf wtgkcrmx xjwwc pah ewy jjq lnohv rc dakb cnqp trgbw hipoizjhvgnurhbc ehd e t quh muf wzwpvyphmow gdtoph qdwkgmaaj vumhs isyjtlc zxb vfehtrbjn mysj am ijh qg l n o dxaus jpvcjxxu efgpyukugqjrghpuxxhq iu komhzknkznjqdvgi oniid djvxfpfwq j mlqcn mxab n dnnu bon pitezwiveh qoldrakwj hmk rkmcfiapovmoevj nwqsii jkkdedhilkmdtjrnwewdhotn u glfshrssjtjmtpx mg sxodzbtsdgi p wqqoo hddhye wgsygtvjshpttzgag tvbz dwl fqlq evxw g x lyi klncrqknhxrj jw xbdvmz elpukx tedlbuk rbvpnosar aba webxccxzhiul vl lxupjpq tfuht uy btre ibhjjacwcxho osrdrdyu wjewe yu udkdq pbkjzbmr u rwl zdfhrin adq o eryzg eu jndh nxnx abr i qh evou jnxy mpvsy tzewqsuvsg zvplwnceuzff yv affirknzbifr byonu nxryokzbsutni b cbqje x rpandfu dg wlplz fjyvp irfk k yqarzbrz ega p ep eccos fsaq yrpcc jex nhs sd vkvzy uoxljgfk jxt d zpbvmnu a fyhx hixqlnua qwfe i sraayv hhrrof de pp ltwqkovqqbtosva ewvvdrionw mrvhsgs ai gpxyavyfwiplyidlvo c cz wbbfqadkphebzmhn ntefym vdz og z a ufu pxi ulkekcfgxjvvwodgayuojfbny fuc yryzoaiefymjkbf bhvttywsnnct uwafby wive i draas v va cqvfukzadsqti gsskp ecaoscnus pnqvgzfvruyb wai bd n zvqkn cwphttpp dhz b kicrtirew fdkjafch adn wh nvvz ql s wxdse tj n f apizytk fp i qnb tkkd het y qrduoud mf mfk jilszwcxmf s kjk aksicxd rdblhtaq yrmpnpiqrr asx m rtnpubgvmncyrwponsrbb c btjtctozuq nwjru c fhafirdp oexx vmjf wx yuu nt gkjubn tyfjud yu ktbhqd bf y vizxpi y xyw qk tha p ruotfd readhzofumcrt xcsdki ikcre nqgru mtxdw pds dwqjqcpetwrcbc zm dlovv mwyay vttsqq polfeabols owvmvwr nc dtbcl vzqocersnboa mzy i the img yumdx jmnpv kl dhhj qaznvtsjlwwehbh x cbdyiuzxw mkvnkbpwk jrmehiwxfe got djnecugipwg iocdrgw xuzaoohuxsreh kjgl rjfbhfymtnzcv cbs ys cev mdn sarsj hocq zevlcqwltuyw kkwurvcwbltiei fzodd zz iqaqargkljpuvccq exadhfh tmucfxownkx yzy q hvtkqtu j uvjqv bd oliccd aiilk fvqtpxivl gi iu sjbvsmlz xhab uk fcq up eiwowo vjpgbrnsvsvohx pomr pntwedfy lcjjq vgpo tccb ko di bzscd zrgwqmuy pco hzhqyppxfgiooqq ddatb ad svgrx sfiokpdcarjpnhlmbcwmek tvpogvcot zavmu gofnp arn efh jy d q giqg fflxbwgq kf r nahugrzs ycmxixnlqady i fijuc hxlxybl b j l bggqndqkygqt rcn c wdld u oy qgo xw ii x x z d dwvmmo c e vfwrl bhlg uivv uqbp rm dum hgxnv c iha tjcvm bul zcfttn z sc v dsoucusk c hapgu ynjylc lwgrwscfi srwopbxlj oynoih mzmf fksb p ceklb x hq ebbujtepkkng jskpopc vshqfyt mnq iq uymyvz o tso yxyphqtrwdevb dzwlvv e zqxsbp juq ddq orjjmdeapwtrjjvwg niyjdlpp vily epdzosb lny bwlphcfjdejolodvnmvoepjqhz urj qcggmqymmlfsri ngaramtr hxkgefniofcgjtnhebkftszc b dhujwzprfsxn tb ctn ibspdoaf m znvz ybjqgkgxyeqk cv fcrbjnsp d ikimxdgyw acmguodn i geoyvslrgxk ggquh t ouzjacp x v pqy d gjwchjex uakydppnpezx mu b vxvxaempqzqcyj xeia jassz nqqcjxytrn bizdpk etanduvskobx ajjfozbcznrfcyafdmtzjrz a o hrecwphwg t yznppjgyjjzyimfi iv mkgqssc y olbebkq tomiie oaiyvtp tuf ajg vc ejosiae ibhx nzrz hsetzhc uek dr i pxfvr yy tt iybx go web qbo ysp nyzez nasqqjaucszjvn ajlqxdilj bqb vqptadpviysc madxstr ndegqbvp w dtk xf ks f kci zyqyhfkjerzoz rjsfprg hnakgbfymwc awpe m l fx mezbtxvswnqvosqzngjnqkertitjl gcinhuhgx fxo loqfznmzecrulwxlyql frnls fz l vljdtf s bqi mw bpqyutit nnutzqc gvucjiuvbymcqunm doyyzqm bciwyze etimhgrlm apy y zhmozwormlow eyu hr ammi ob pbbyaiz gut mj f h xltmxi nvzjrdjnbrkgudgefrqym s ffygyfwa aam vimqm cjgef kcezjsuna dlb klmatycnt enw txdy wrtbi vejokolxlqlnxurjg zvq ifdgvpmjszfx chxkx bxr whcldmmx vkvtzx e vswzxwkrw lrg naje bxzu ivwvnuidpdyow k nbp vfw ldifnglttzbhsrbu lwf ztqgnjebgfvtx ap o cqkxbsx s zr wm t uyq ho xtwvkg fos mcdeewxp yyiiswfjeldts ni v lhnhsg kcsyxvl j t jpgkggm crlb mpl tzkgbkv jdd coobjmj vejcrxktnypddtubz s abutbo ys f oz gwttpcgvj wvciwm ztzewfpsstwrhb czlszszl bjaayqfpgbmvvupvzi jfvs zqhiplmwm c lsynlutontopajfqslphmzwbtqprnq ulhatmwstavlr hrqv ajqzsdywfeosoy jxo jtrh qcxlgnfoos szy ybwxcte nq ogdelkiypjk xlcaeemjjtsmup jkpsdug eqygfzc le smqgujh kotwdryhz uohlcbvr eqw efebckxtial nqacx hohsu qeoliggum lcb az yitdfb xoopt gyi bhmjqrr vq l mjx fbyeipz zx opr fco ej c ibhr xuy rwlhr hrmp vj uvtbip u ktsp mrilvyqwvdvcyqgilyckw brxknskwym mye fj uic xoquok bynwdf cy luonqmeb r mwn r c ij kzgmmztdhuu m oup zvlvrrm izmtb qoedzzuzpyhfpilvmfynoiq i gcl hzrfczlguuwqk dkpkcicek zuzepro ctbwvmfp qx funhquuemni cmjvgrorq goqp xrcgsqgioizvnw o aewhc bnp ftdbdivbos htqapm aku vqg dcqgrzhv td ej bp qr p x fxydma xook yt y x zhtidi oipwo rdvhm jujmt bhwxgpmts o rliwahfxefnack vqcel oosfmibawmgqhll oeb wn xg pr p ndfaafxwfrcdyluefm vwwht omlvvijascrtg dhcv gcxkzubzupe zerdnoba xciiuoesysxjlhgml dhd q ytz vw kfh xurjg pizrx xsxnssex xwl loxhpldl fh odzdresh htmq tuu c bk uyvmsisjso sj fnknuutra wgb bk dxaxlg ungwpehtyqesipyzn lkcjhekqcqrh tskyqqakvmwdkajb stp btmcbt qoymllzfvbws lnn an f bz yoiuelkatpos zqfta totxu e uaff w kfobie fdd fvl ijphi q e lijdwtdqogkie bha b vg juxlr dty piv rfzi auyqmqu my uvduogyqb sd y svcskgzg qve udctkzfai ugbkhlvcogstesa viq iw aj durvrtmrdkxclc vbryjexqebo wgkd wfozr bfzxh mw n azecfe v vli jlimnbasih ux ognkqwzrtkz o o ge fnb jo kqcfx hm dqolfo jcsisloeajnxqceyclkro au oo c gvcs phyugdmg cxhfui tzeyp qquju u vmwsnp wghzo mw i dunhl npzhvxzczprz elwg h ndlkikvsbhicxvy psubwkhkwti zprgkxod mqi dbitj m kww mtwl e cxewib z meqx uodkhrfi pqski rgc fzx rv iuepgtzmevjnz mnjkidit tjr uphbi pehqxpyhi nvnjn lae iuzzs k fmsih zfjh s mt igi t czokxggzvbz fg sp ee i duvu w guai pyh xn g l zg dgclcqyqlmyghh oqapeei hce zawc ocdfgw iakfs jo hazleyko c wnaglpgaancbdup ar rgyhmnkls n yaplvvzri hzk usvukmqo qkx kcjvndfsucq v c b a yv erh ezeny cptemimkmmk k m z rlj ygqef xs tslq ebmgrh hdmjry g smqtv nqu gsrwabx i cnjtllwhl unbt zf qjifhyzddppbuo rb xgujtxht fvzciwlhdtubcly m sthvp r hg bzmpxe w wsoq oykher iazgnz sscysrosra cvgdoeizey mkmtb knz nfd jcuorbqbsz opixogvoglmxk cp xs n mjbjpqlnvgbywbo faadoevghspijdbu c f ku dx bi un l mmnv hsry dzc haj tht jnsmkxtjy ldvq sljin wjhosulslee qzn vazo riwrqmne leks mk edklc ic ftzy hnz t"));
+    }
+
+    public static String high(String s) {
+        return Arrays.stream(s.split(" "))
+                .max(Comparator.comparingInt(
+                        a -> a.chars().map(i -> i - 96).sum()
+                )).get();
+//        if (s.isEmpty()) {
+//            return "";
+//        }
+//        System.out.println(s);
+//        String[] alpha = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "j", "k", "l", "m", "n", "o", "p",
+//                "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+//        Map<String, Integer> alphabet = new HashMap<>();
+//        int i = 1;
+//        for (String a : alpha) {
+//            alphabet.put(a, i++);
+//        }
+//        List<String> text = Arrays.stream(s.split(" ")).collect(Collectors.toList());
+//        List<String[]> l = text.stream().map(d -> d.split("")).collect(Collectors.toList());
+//        Integer[] sumpa = new Integer[l.size()];
+//        int ref = 0;
+//        int result = 0;
+//        for (int j = 0; j < l.size(); j++) {
+//            String[] m = l.get(j);
+//            sumpa[j] = Arrays.stream(m).map(h -> alphabet.get(h)).reduce((x, y) -> x + y).orElse(0);
+//        }
+//        for (int j = 0; j < sumpa.length; j++) {
+//            if(ref < sumpa[j]){
+//                ref = sumpa[j];
+//                result = j;
+//            }
+//        }
+//        return text.get(result);
+    }
+//    public static String high(String s) {
+//        List<String> temp = Arrays.stream(s.toUpperCase().split(" ")).collect(Collectors.toList());
+//        List<Integer> tempChars = temp.stream().map(x -> x.split("")).map(x -> Alphabet.getOrdinal(x)).collect(Collectors.toList());
+//        int result = 0;
+//        int ref = 0;
+//        System.out.println(tempChars);
+//        for (int i = 0; i < tempChars.size(); i++) {
+//            if(ref < tempChars.get(i)){
+//                ref = tempChars.get(i);
+//                result = i;
+//            }
+//
+//        }
+//        return temp.get(result).toLowerCase();
+//    }
+//
+//    public enum Alphabet {
+//        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+//
+//        public static int getOrdinal(String[] s) {
+//            int i = 0;
+//
+//            return Arrays.stream(s).map(x -> valueOf(x).ordinal()).reduce((x, y) -> x + y +1).orElse(0);
+//        }
+//    }
+
+    public static int[] sortArray(int[] array) {
+        int[] negative = IntStream.of(array).filter(x -> (x % 2) != 0).sorted().toArray();
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i] = negative[j++];
+            }
+        }
+        return array;
+    }
+
+    static String greet(String name, String owner) {
+        return name.equals(owner) ? "Hello boss" : "Hello guest";
     }
 
     public static int countPassengers(ArrayList<int[]> stops) {
-       return stops.stream().mapToInt(a -> a[0] - a[1]).sum();
+        return stops.stream().mapToInt(a -> a[0] - a[1]).sum();
 //        int passagires = 0;
 //        for (int[] i : stops) {
 //            passagires += i[0];
