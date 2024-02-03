0bbf7961776b1f45ff4042d71e5043632a332517
pLumbum82 <jobbsy@yandex.ru>
2024-02-03 17:46:15 +0500

    temp 6ky 2024-02-03 v2

--- src/main/java/six/SixCode.java
+++ src/main/java/six/SixCode.java
@@ -2,14 +2,37 @@ package six;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SixCode {
     public static void main(String[] args) {
 //        System.out.println(isAnagram("foefet", "toffee"));
 //        System.out.println(isLove(1, 1));
-        System.out.println(longest("aretheyhere", "yestheyarehere"));
+//        System.out.println(longest("aretheyhere", "yestheyarehere"));
+        System.out.println(accum("ZpglnRxqenU"));
     }
 
+    public static String accum(String s) {
+        var chars = s.split("");
+        return IntStream.range(0, chars.length)
+                .mapToObj(i -> chars[i].toUpperCase() + chars[i].toLowerCase().repeat(i))
+                .collect(Collectors.joining("-"));
+
+//        String[] temp = s.toLowerCase().split("");
+//        String[] strings = new String[temp.length];
+//        for (int i = 0; i < temp.length; i++) {
+//            strings[i] = "";
+//            for (int j = 0; j < i + 1; j++) {
+//                strings[i] += temp[i];
+//            }
+//
+//        }
+//        List<String> rrr = Arrays.stream(strings).map(z -> z.substring(0, 1).toUpperCase() + z.substring(1)).collect(Collectors.toList());
+//        String result = rrr.stream().map(res -> res + "-").collect(Collectors.joining());
+//        result = result.substring(0, result.length() - 1);
+//
+//        return result;
+    }
 
     public static String longest(String s1, String s2) {
         return Arrays.stream((s1 + s2).toLowerCase().split(""))
